1c815172736f3c06ea47f4dce43e3a9bb000a8e9
Rohan Awhad <rohanavad007@gmail.com>
2023-11-15 14:56:55 -0700

    Filter links based on current URL path.

--- crawler/custom_spider.py
+++ crawler/custom_spider.py
@@ -10,11 +10,34 @@ class CustomSpider(scrapy.Spider):
         super().__init__(**kwargs)
 
     def parse(self, response):
+        # get response url
+        curr_link = response.url
+        filter_link = True
+        if curr_link.endswith('/'):
+            if (curr_link[:-1]).endswith(self.path):
+                filter_link = False
+        else:
+            if curr_link.endswith(self.path):
+                filter_link = False
+
         # Get the current depth (default to 0 if not set)
         current_depth = response.meta.get('depth', 0)
 
         links = response.css('a::attr(href)').getall()
-        filtered_links = [link for link in links if link.startswith(self.path)]
+        if filter_link:
+            filtered_links = [link for link in links if link.startswith(self.path)]
+        else:
+            filtered_links = [link for link in links 
+                              if not (link.startswith('/') or
+                                      link.startswith('http') or
+                                      link.startswith('https') or
+                                      link.startswith('mailto') or
+                                      link.startswith('tel') or
+                                      link.startswith('javascript') or
+                                      link.startswith('data') or
+                                      link.startswith('ftp') or
+                                      link.startswith('file') or
+                                      link.startswith('#'))]
         yield {'url': response.url, 'links': filtered_links}
 
         # Follow links only if depth is less than 2
