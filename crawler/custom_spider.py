61b1ab55be131d7292e60bf1142054f283638f35
Rohan Awhad <rohanavad007@gmail.com>
2023-11-15 14:08:32 -0700

    Add path filtering to CustomSpider constructor

--- crawler/custom_spider.py
+++ crawler/custom_spider.py
@@ -4,10 +4,12 @@ import scrapy
 class CustomSpider(scrapy.Spider):
     name = 'custom_spider'
 
-    def __init__(self, start_url, **kwargs):
+    def __init__(self, start_url, path, **kwargs):
         self.start_urls = [start_url]
+        self.path = path
         super().__init__(**kwargs)
 
     def parse(self, response):
         links = response.css('a::attr(href)').getall()
-        yield {'url': response.url, 'links': links}
+        filtered_links = [link for link in links if link.startswith(self.path)]
+        yield {'url': response.url, 'links': filtered_links}
