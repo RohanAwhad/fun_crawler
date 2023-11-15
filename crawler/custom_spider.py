0404d96fe5cffdaa37913346fe223643c504993d
Rohan Awhad <rohanavad007@gmail.com>
2023-11-15 16:06:29 -0700

    Refactor crawler to include max depth parameter

--- crawler/custom_spider.py
+++ crawler/custom_spider.py
@@ -1,19 +1,21 @@
 # spiders/custom_spider.py
 import scrapy
 
-class CustomSpider(scrapy.Spider):
-    name = 'custom_spider'
 
-    def __init__(self, start_url, path, **kwargs):
+class CustomSpider(scrapy.Spider):
+    name = "custom_spider"
+
+    def __init__(self, start_url, path, max_depth, **kwargs):
         self.start_urls = [start_url]
         self.path = path
+        self.max_depth = int(max_depth)
         super().__init__(**kwargs)
 
     def parse(self, response):
         # get response url
         curr_link = response.url
         filter_link = True
-        if curr_link.endswith('/'):
+        if curr_link.endswith("/"):
             if (curr_link[:-1]).endswith(self.path):
                 filter_link = False
         else:
@@ -21,26 +23,35 @@ class CustomSpider(scrapy.Spider):
                 filter_link = False
 
         # Get the current depth (default to 0 if not set)
-        current_depth = response.meta.get('depth', 0)
+        current_depth = response.meta.get("depth", 0)
+        if current_depth == 0:
+            filter_link = True
 
-        links = response.css('a::attr(href)').getall()
+        links = response.css("a::attr(href)").getall()
         if filter_link:
             filtered_links = [link for link in links if link.startswith(self.path)]
         else:
-            filtered_links = [link for link in links 
-                              if not (link.startswith('/') or
-                                      link.startswith('http') or
-                                      link.startswith('https') or
-                                      link.startswith('mailto') or
-                                      link.startswith('tel') or
-                                      link.startswith('javascript') or
-                                      link.startswith('data') or
-                                      link.startswith('ftp') or
-                                      link.startswith('file') or
-                                      link.startswith('#'))]
-        yield {'url': response.url, 'links': filtered_links}
+            filtered_links = [
+                link
+                for link in links
+                if not (
+                    link.startswith("/")
+                    or link.startswith("http")
+                    or link.startswith("https")
+                    or link.startswith("mailto")
+                    or link.startswith("tel")
+                    or link.startswith("javascript")
+                    or link.startswith("data")
+                    or link.startswith("ftp")
+                    or link.startswith("file")
+                    or link.startswith("#")
+                )
+            ]
+        yield {"url": response.url, "links": filtered_links}
 
         # Follow links only if depth is less than 2
-        if current_depth < 2:
+        if current_depth < int(self.max_depth):
             for link in filtered_links:
-                yield response.follow(link, self.parse, meta={'depth': current_depth + 1})
+                yield response.follow(
+                    link, self.parse, meta={"depth": current_depth + 1}
+                )
--- crawler/main.py
+++ crawler/main.py
@@ -1,4 +1,3 @@
-# main.py
 from fastapi import FastAPI, HTTPException
 import subprocess
 import json
@@ -10,40 +9,57 @@ app = FastAPI()
 
 # Initialize Redis client
 redis_client = redis.Redis(
-    host=os.getenv('REDIS_HOST', 'localhost'), 
-    port=os.getenv('REDIS_PORT', 6379), 
-    db=int(os.getenv('REDIS_DB', 0))
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=os.getenv("REDIS_PORT", 6379),
+    db=int(os.getenv("REDIS_DB", 0)),
 )
 
+
 @app.get("/crawl")
-async def crawl(url: str, path: str):
+async def crawl(url: str, path: str, max_depth: int = 1):
     # Create a unique key based on URL and path
-    unique_key = hashlib.sha256(f'{url}{path}'.encode()).hexdigest()
+    unique_key = hashlib.sha256(f"{url}{path}{max_depth}".encode()).hexdigest()
 
     # Check if the data is in cache
     if (cached_data := redis_client.get(unique_key)) is not None:
         return json.loads(cached_data)
 
-
     # Call the Scrapy spider using subprocess
     process = subprocess.Popen(
-        ['python3', '-m', 'scrapy', 'runspider', 'custom_spider.py', '-a', f'start_url={url}', '-a', f'path={path}', '-O', 'output.json'],
+        [
+            "python3",
+            "-m",
+            "scrapy",
+            "runspider",
+            "custom_spider.py",
+            "-a",
+            f"start_url={url}",
+            "-a",
+            f"path={path}",
+            "-a",
+            f"max_depth={max_depth}",
+            "-O",
+            "output.json",
+        ],
         stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
     )
     stdout, stderr = process.communicate()
 
     if process.returncode != 0:
-        if stderr: err = stderr.decode()
-        else: err = stdout.decode()
+        if stderr:
+            err = stderr.decode()
+        else:
+            err = stdout.decode()
         raise HTTPException(status_code=500, detail=err)
-
 
     # Read the results from the file
     try:
-        with open('output.json') as f: data = json.load(f)
+        with open("output.json") as f:
+            data = json.load(f)
         redis_client.set(unique_key, json.dumps(data))
         return data
     except Exception as e:
-        with open('output.json') as f: data = f.read()
+        with open("output.json") as f:
+            data = f.read()
         return {"data": data, "error": str(e)}
