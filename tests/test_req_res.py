c05f1e8a70def5f10a7217587089f53c8238e28b
Rohan Awhad <rohanavad007@gmail.com>
2023-12-11 12:11:07 -0700

    Add test for successful requests and failed requests

---
+++ tests/test_req_res.py
@@ -0,0 +1,35 @@
+# Success
+
+reqs = [
+    "https://tempepethospital.com/",
+]
+
+responses = [
+    {
+        "data": [
+            "https://tempepethospital.com",
+            "https://tempepethospital.com/",
+            "https://tempepethospital.com/cat-condos/",
+            "https://tempepethospital.com/contact/",
+            "https://tempepethospital.com/dental/",
+            "https://tempepethospital.com/doggie-day-camp-doggie-day-care/",
+            "https://tempepethospital.com/employment-application/",
+            "https://tempepethospital.com/our-team/",
+            "https://tempepethospital.com/pet-plans/",
+            "https://tempepethospital.com/privacy-policy/",
+            "https://tempepethospital.com/services/",
+            "https://tempepethospital.com/services/grooming/",
+            "https://tempepethospital.com/services/pet-resort/",
+            "https://tempepethospital.com/services/price-list",
+            "https://tempepethospital.com/services/price-list/",
+            "https://tempepethospital.com/terms-of-use/",
+            "https://tempepethospital.com/tour-tlvc/"
+        ],
+        "error": None
+    }
+]
+
+# Fails:
+failed_reqs = [
+    "https://www.petco.com",
+]
